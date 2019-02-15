c4dbc6bb7759022e3456d08ef48f2472c8d5b1da
Nicholas Yip <nicholasyip@obceit.cc>
2019-02-16 02:55:54 +0900

    Enumerate for method references

--- src/main/scala/io/github/portfoligno/dex/permission/PermissionAnalyzer.scala
+++ src/main/scala/io/github/portfoligno/dex/permission/PermissionAnalyzer.scala
@@ -4,15 +4,29 @@ import java.io.File
 
 import io.github.portfoligno.dex.permission.settings.MappingSource
 import org.jf.dexlib2.DexFileFactory
+import org.jf.dexlib2.iface.instruction.ReferenceInstruction
+import org.jf.dexlib2.iface.reference.MethodReference
 
 import scala.collection.convert.ImplicitConversionsToScala._
+import scala.reflect.ClassTag
 
 object PermissionAnalyzer {
+  private
+  val ReferenceInstruction = implicitly[ClassTag[ReferenceInstruction]]
+  private
+  val MethodReference = implicitly[ClassTag[MethodReference]]
+
   def analyzeDexContainer[F[_]](file: File, mappingSource: MappingSource): Unit = {
     val container = DexFileFactory.loadDexContainer(file, null)
 
     container
       .getDexEntryNames
-      .map(container.getEntry)
+      .view
+      .flatMap(container.getEntry(_).getClasses.toSeq)
+      .flatMap(_.getMethods)
+      .flatMap(m => Option(m.getImplementation))
+      .flatMap(_.getInstructions)
+      .flatMap(ReferenceInstruction.unapply)
+      .flatMap(i => MethodReference.unapply(i.getReference))
   }
 }
--- src/main/scala/io/github/portfoligno/dex/permission/settings/package.scala
+++ src/main/scala/io/github/portfoligno/dex/permission/settings/package.scala
@@ -2,6 +2,7 @@ package io.github.portfoligno.dex.permission
 
 package object settings {
   private[settings] val CONTENT_PROVIDER = "CONTENT_PROVIDER"
+
   private[settings] val ContentProvider = "android.content.ContentProvider"
   private[settings] val Uri = "android.net.Uri"
   private[settings] val String = "java.lang.String"
